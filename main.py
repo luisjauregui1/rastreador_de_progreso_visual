6a62cebd2468207d60c436adf2a77a959b3cdd49
Gustavo Fernandez <sr1176626173@gmail.com>
2025-03-12 18:54:59 -0600

    Primer commit - agregando archivos iniciales

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import messagebox
+import json
+import os
+
+BACKGROUD_COLOR = '#f4f4f4'
+
+# Ventana princial
+window = Tk()
+window.title("rastreador de progreso visual")
+window.config(padx=50, pady=50, bg=BACKGROUD_COLOR)
